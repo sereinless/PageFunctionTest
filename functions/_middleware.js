09f96da86cb37d04fa0327ad0b6fe3f12b3056bc
sereinless <86793691+sereinless@users.noreply.github.com>
2022-07-07 00:05:41 +0800

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -10,6 +10,6 @@ export async function onRequest(context) {
     } = context;
 
     let url = new URL(request.url);
-    url.hostname = "https://curlconverter.com/";//该成自己的域名
+    url.hostname = "curlconverter.com";//该成自己的域名
     return fetch(new Request(url, request))
 }
